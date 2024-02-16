7cc32a6e6b99a8e9ff5bea03357a2cf5a4b2a4af
Iury Gomes de Oliveira <iurygdeoliveira@gmail.com>
2024-02-16 14:36:02 +0000

    ajustes

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var menuButton = document.querySelector(".menu-button");
-    var sidebar = document.querySelector(".sidebar");
-    menuButton.addEventListener("click", function () {
-        sidebar.classList.toggle("hide-sidebar");
-    });
-});
