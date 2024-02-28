9d483e06206ce438776421bf9326de945c4ae452
Jhon HR <jhonlene12@gmail.com>
2024-02-28 16:45:47 -0300

    Alterações no layout com responsividade, e adição de uma navbar responsiva

---
+++ script.js
@@ -0,0 +1,12 @@
+// Função para abrir a navbar inserindo um style com 150px de largura, e empurrando main a 150px da esquerda
+function openNav() {
+    document.getElementById("left-navbar").style.width = "150px";
+    document.getElementById("main").style.marginLeft = "150px";
+  }
+  
+//função para fechar a navbar zerando a largura da navbar e voltando o main para 0px da esquerda 
+function closeNav() {
+    document.getElementById("left-navbar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+  }
