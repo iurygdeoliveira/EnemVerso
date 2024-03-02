7db1ced4da0572a773c9874a0ac05ff0e9b18ed1
Jhon HR <jhonlene12@gmail.com>
2024-03-02 11:53:53 -0300

    alterações na navbar lateral e alterações no js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-// Função para abrir a navbar inserindo um style com 150px de largura, e empurrando main a 150px da esquerda
+// Função para abrir a navbar inserindo um style com 150px de largura e 
 function openNav() {
-    document.getElementById("left-navbar").style.width = "160px";
-    // document.getElementById("main").style.marginLeft = "50px";
+    document.getElementById("left-navbar").style.width = "150px";
+    document.getElementById("conteiner").style.backgroundColor = "rgba(1,1,1,0.1)";
   }
   
 //função para fechar a navbar zerando a largura da navbar e voltando o main para 0px da esquerda 
 function closeNav() {
     document.getElementById("left-navbar").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
-    document.body.style.backgroundColor = "white";
   }
