5e35934351d8de034ce6d9fcaebc16cade8c6c9f
Juan Felipe Alves Flores <juan.felipe.alves.flores@gmail.com>
2024-05-15 15:20:36 -0300

    :sparkles: feat: Adicionada melhoria nos gráficos.

--- grafico.js
+++ grafico.js
@@ -1,41 +1,48 @@
-  var chartDom = document.getElementById('grafico');
-  var myChart = echarts.init(chartDom);
-  var option;
-  option = {
-    title: {
-      text: 'Basic Radar Chart'
-    },
-    legend: {
-      data: ['Allocated Budget', 'Actual Spending']
-    },
-    radar: {
-      shape: 'circle',
-      center: ['50%', '50%'],
-      radius: 100,
-      indicator: [
-        { name: 'Eixo 1', max: 100 },
-        { name: 'Eixo 2', max: 100 },
-        { name: 'Eixo 3', max: 100 },
-        { name: 'Eixo 4', max: 100 },
-        { name: 'Eixo 5', max: 100 },
-      ]
-    },
-    series: [
-      {
-        name: 'Budget vs spending',
-        type: 'radar',
-        data: [
-          {
-            value: [42, 30, 20, 35, 50],
-            name: 'Allocated Budget'
-          },
-          {
-            value: [50, 14, 28, 26, 42],
-            name: 'Actual Spending'
-          }
-        ]
-      }
-    ]
-  };
+var chartDom = document.getElementById('grafico');
 
-  option && myChart.setOption(option);
+var myChart = echarts.init(chartDom);
+
+var option;
+
+option = {
+  title: {
+    text: 'Consigo relacionar obras e artistas específicos às suas respectivas vanguardas artísticas?',
+    textStyle: {
+      fontSize: 14,
+    }
+  },
+  legend: {
+    data: ['TADS', 'Ensino Médio'],
+    bottom: 0
+  },
+  radar: {
+    shape: 'circle',
+    center: ['50%', '50%'],
+    radius: 100,
+    indicator: [
+      { name: 'Indiferente', max: 5 },
+      { name: 'Discordo', max: 5 },
+      { name: 'Discordo totalmente', max: 5 },
+      { name: 'Concordo totalmente', max: 5 },
+      { name: 'Concordo', max: 5 },
+    ]
+  },
+  series: [
+    {
+      name: 'TADS vs Ensino Médio',
+      type: 'radar',
+      data: [
+        {
+          value: [3, 4, 2, 1, 5, 1, 5, 4, 3, 4],
+          name: 'TADS'
+        },
+        {
+          value: [5, 4, 1, 3, 4, 2, 5, 1, 4, 5],
+          name: 'Ensino Médio'
+        }
+      ]
+    }
+  ]
+};
+
+option && myChart.setOption(option);
