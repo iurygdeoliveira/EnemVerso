c5e7b136f56594e78840fd5e5d165d6397484631
Victor Ferraz <107584702+victorferrazms@users.noreply.github.com>
2024-05-17 00:57:14 +0000

    att 2

--- grafico.js
+++ grafico.js
@@ -8,7 +8,7 @@ option = {
         top: '3%',
     },
     legend: {
-        data: ['Questão1', 'Questão2', 'Questão3', 'Questão4', 'Questão5','Questão6','Questão7','Questão8','Questão9','Questão10','Questão11','Questão12','Questão13','Questão14','Questão15']
+        data: ['orçamento alocado', 'gastos reais']
     },
     radar: {
         shape: 'circle',
@@ -28,48 +28,12 @@ option = {
             type: 'radar',
             data: [
                 {
-                    value: [5, 3, 5, 5, 5, 5, 5, 5, 5, 5, 5],
-                    name: 'Questão1'
+                    value: [5, 3, 5, 5, 5, 5, 5],
+                    name: 'orçamento alocado'
                 },
                 {
-                    value: [5, 3, 5, 5, 2, 5, 1, 5, 5, 5, 5],
-                    name: 'Fake News e Propagação Online'
-                },
-                {
-                    value: [5, 3, 5, 4, 5, 5, 5, 3, 5, 5, 5],
-                    name: 'Influência das IA na Sociedade'
-                },
-                {
-                    value: [5, 3, 5, 2, 5, 5, 1, 5, 5, 5, 5],
-                    name: 'Sociedade da Informação'
-                },
-                {
-                    value: [5, 3, 5, 5, 1, 5, 5, 5, 3, 5, 5],
-                    name: 'Transformação Cultural pelas TIC'
-                },
-                {
-                    value: [5, 3, 5, 5, 5, 5, 5, 5, 5, 5, 5],
-                    name: 'Marketing e Publicidade Online'
-                },
-                {
-                    value: [5, 3, 5, 5, 5, 5, 5, 5, 5, 5, 5],
-                    name: 'Impacto Sociocultural das TIC'
-                },
-                {
-                    value: [5, 3, 5, 5, 5, 5, 5, 4, 5, 5, 4],
-                    name: 'Redes Sociais e Influência Digital'
-                },
-                {
-                    value: [5, 1, 5, 5, 5, 5, 2, 5, 5, 5, 3],
-                    name: 'Acesso e Inclusão Social'
-                },
-                {
-                    value: [5, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5],
-                    name: 'Tendências do Mercado de Trabalho'
-                },
-                {
-                    value: [5, 3, 5, 5, 3, 4, 1, 0, 5, 5, 5],
-                    name: 'Impacto das TIC nas Profissões'
+                    value: [5, 5, 1, 5, 5, 5, 5],
+                    name: 'gastos reais'
                 },
             ]
         }
