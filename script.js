917705912c3f72eec729d93063a6bb28da172d7c
Paulo Ricardo <paulo.silva56@estudante.ifto.edu.br>
2024-05-17 14:31:19 -0300

    Ainda nn tá pronto! Falta terminar de implementar os gráficos.

---
+++ script.js
@@ -0,0 +1,43 @@
+import * as echarts from 'echarts'
+
+const div1 = document.getElementById('grafico_3b')
+const graph1 = echarts.init()
+const options1 = {
+
+    title: {
+        text: 'Gráfico 1'
+      },
+      legend: {
+        data: ['Turma Alpha', 'Turma Beta']
+      },
+      radar: {
+        // shape: 'circle',
+        indicator: [
+          { name: 'Sales', max: 6500 },
+          { name: 'Administration', max: 16000 },
+          { name: 'Information Technology', max: 30000 },
+          { name: 'Customer Support', max: 38000 },
+          { name: 'Development', max: 52000 },
+          { name: 'Marketing', max: 25000 }
+        ]
+      },
+      series: [
+        {
+          name: 'Budget vs spending',
+          type: 'radar',
+          data: [
+            {
+              value: [4200, 3000, 20000, 35000, 50000, 18000],
+              name: 'Allocated Budget'
+            },
+            {
+              value: [5000, 14000, 28000, 26000, 42000, 21000],
+              name: 'Actual Spending'
+            }
+          ]
+        }
+      ]
+
+}
+
+graph1.setOption(options1)
