f25c87991b2d0cb066ac32763148c7f6df3d3de0
displinter <splinter.guilherme@gmail.com>
2024-05-25 14:47:03 +0000

    graficos

---
+++ script.js
@@ -0,0 +1,129 @@
+function drawGrafico1 () {
+
+  var chartDom = document.getElementById('graph1');
+  var myChart = echarts.init(chartDom);
+  var option;
+
+  option = {
+    title: {
+      text: 'Basic Radar Chart'
+    },
+    legend: {
+      data: ['Allocated Budget', 'Actual Spending']
+    },
+    radar: {
+      // shape: 'circle',
+      indicator: [
+        { name: 'Sales', max: 6500 },
+        { name: 'Administration', max: 16000 },
+        { name: 'Information Technology', max: 30000 },
+        { name: 'Customer Support', max: 38000 },
+        { name: 'Development', max: 52000 },
+        { name: 'Marketing', max: 25000 }
+      ]
+    },
+    series: [
+      {
+        name: 'Budget vs spending',
+        type: 'radar',
+        data: [
+          {
+            value: [4500, 7000, 20400, 35000, 50000, 6000],
+            name: 'Allocated Budget'
+          }
+        ]
+      }
+    ]
+  };
+
+  option && myChart.setOption(option);
+
+}
+
+function drawGrafico2 () {
+
+  var chartDom = document.getElementById('graph2');
+  var myChart = echarts.init(chartDom);
+  var option;
+
+  option = {
+    title: {
+      text: 'Basic Radar Chart'
+    },
+    legend: {
+      data: ['Allocated Budget', 'Actual Spending']
+    },
+    radar: {
+      // shape: 'circle',
+      indicator: [
+        { name: 'Sales', max: 6500 },
+        { name: 'Administration', max: 16000 },
+        { name: 'Information Technology', max: 30000 },
+        { name: 'Customer Support', max: 38000 },
+        { name: 'Development', max: 52000 },
+        { name: 'Marketing', max: 25000 }
+      ]
+    },
+    series: [
+      {
+        name: 'Budget vs spending',
+        type: 'radar',
+        data: [
+          {
+            value: [4500, 7000, 20400, 35000, 50000, 6000],
+            name: 'Allocated Budget'
+          }
+        ]
+      }
+    ]
+  };
+
+  option && myChart.setOption(option);
+
+}
+
+function drawGrafico3 () {
+
+  var chartDom = document.getElementById('graph3');
+  var myChart = echarts.init(chartDom);
+  var option;
+
+  option = {
+    title: {
+      text: 'Basic Radar Chart'
+    },
+    legend: {
+      data: ['Allocated Budget', 'Actual Spending']
+    },
+    radar: {
+      // shape: 'circle',
+      indicator: [
+        { name: 'Sales', max: 6500 },
+        { name: 'Administration', max: 16000 },
+        { name: 'Information Technology', max: 30000 },
+        { name: 'Customer Support', max: 38000 },
+        { name: 'Development', max: 52000 },
+        { name: 'Marketing', max: 25000 }
+      ]
+    },
+    series: [
+      {
+        name: 'Budget vs spending',
+        type: 'radar',
+        data: [
+          {
+            value: [4500, 7000, 20400, 35000, 50000, 6000],
+            name: 'Allocated Budget'
+          }
+        ]
+      }
+    ]
+  };
+
+  option && myChart.setOption(option);
+
+}
+
+drawGrafico1()
+drawGrafico2()
+drawGrafico3()
