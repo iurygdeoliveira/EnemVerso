416f2c9e885cf56c0d143b7f02cdab22f7acb5b4
Paulo Ricardo <paulo.silva56@estudante.ifto.edu.br>
2024-06-05 19:28:18 -0300

    Dados reais implementados Foram feitas modificações como adição de um sistema que lê, processa e exibe os dados de cada turma. Também foram descritos todas as informações sobre os dados em si, e também sobre os tópicos de cada disciplina. Além disso, agora também há um título que exibe, no topo da página, a matéria selecionada.

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-import { graph_3a, graph_3b, graph_tads } from "./graficos.js"
+import { createGraphics } from "./createGraphics.js"
 
-const div1 = document.getElementById('grafico_3b')
-const div2 = document.getElementById('grafico_3a')
-const div3 = document.getElementById('grafico_tads')
+async function setGraphics ({ graph_3a, graph_3b, graph_tads }) {
 
-const graph1 = echarts.init(div1)
-const graph2 = echarts.init(div2)
-const graph3 = echarts.init(div3)
+    const div1 = document.getElementById('grafico_3b')
+    const div2 = document.getElementById('grafico_3a')
+    const div3 = document.getElementById('grafico_tads')
 
-graph1.setOption(graph_3b)
-graph2.setOption(graph_3a)
-graph3.setOption(graph_tads)
+    const graph1 = echarts.init(div1)
+    const graph2 = echarts.init(div2)
+    const graph3 = echarts.init(div3)
+
+    graph1.setOption(graph_3b)
+    graph2.setOption(graph_3a)
+    graph3.setOption(graph_tads)
+
+}
+
+await setGraphics(await createGraphics('linguaPortuguesa'))
+
+const subButtons = document.getElementsByClassName('subject')
+for (let b = 0; b < subButtons.length; b++) {
+
+    subButtons.item(b).addEventListener('click', async (event) => {
+
+        const subject = document.getElementById('subject')
+        subject.innerHTML = event.target.innerHTML
+
+        const buttonId = event.target.id
+        setGraphics(await createGraphics(buttonId))
+
+    })
+
+}
