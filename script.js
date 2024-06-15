c75690dd2d5b0ffd5e3963b6c23c5cbb172856bb
gabrielbcosta <159305635+gabrielbcosta@users.noreply.github.com>
2024-06-15 22:26:14 +0000

    Graficos implementados com os dados das 3 turmas

--- script.js
+++ script.js
@@ -1,9 +1,50 @@
-function drawChart1() {
+import { materias, topicos } from "/dados_das_turmas.js";
+
+function calcularMedia(dados, respondentes) {
+
+    let somatoria = 0;
+
+    for (let i = 1; i <= 5; i++) {
+
+        somatoria = somatoria + dados[i - 1] * i;
+
+    }
+
+    const media = somatoria / respondentes
+
+    if (media != Infinity) return media
+
+}
+
+function mediasMateria (idMateria, turma) {
+
+    const materia = materias[turma][idMateria]
+    const mediasEixos = []
+    let i = 0
+    while (i < 15) {
+
+        const respondentes = materia[i].reduce((acumulador, atual) => acumulador + atual, 0)
+        const mediaQuestão1 = calcularMedia(materia[i], respondentes)
+        const mediaQuestão2 = calcularMedia(materia[i + 1], respondentes)
+        const mediaQuestão3 = calcularMedia(materia[i + 2], respondentes)
+
+        mediasEixos.push((mediaQuestão1 + mediaQuestão2 + mediaQuestão3) / 3)
+
+        i += 3
+
+    }
+
+    return mediasEixos
+
+}
+
+
+function drawChart1(idMateria) {
 
   var chartDom = document.getElementById('grafico1');
   var myChart = echarts.init(chartDom);
   var option;
-
+  
   option = {
     title: {
       text: '3A INFO'
@@ -14,12 +55,11 @@ function drawChart1() {
     radar: {
       // shape: 'circle',
       indicator: [
-        { name: 'Eixo1', max: 6500 },
-        { name: 'Eixo2', max: 16000 },
-        { name: 'Eixo3', max: 30000 },
-        { name: 'Eixo4', max: 38000 },
-        { name: 'Eixo5', max: 52000 },
-        { name: 'Eixo6', max: 25000 }
+        { name: topicos[idMateria][0], max: 5 },
+        { name: topicos[idMateria][1], max: 5 },
+        { name: topicos[idMateria][2], max: 5 },
+        { name: topicos[idMateria][3], max: 5 },
+        { name: topicos[idMateria][4], max: 5 }
       ]
     },
     series: [
@@ -28,7 +68,7 @@ function drawChart1() {
         type: 'radar',
         data: [
           {
-            value: [5000, 14000, 28000, 26000, 42000, 21000],
+            value: mediasMateria(idMateria, '3A'),
             name: 'Graficos'
           }
         ]
@@ -39,7 +79,8 @@ function drawChart1() {
   option && myChart.setOption(option);
   
 }
-function drawChart2() {
+
+function drawChart2(idMateria) {
 
   var chartDom = document.getElementById('grafico2');
   var myChart = echarts.init(chartDom);
@@ -55,12 +96,11 @@ function drawChart2() {
     radar: {
       // shape: 'circle',
       indicator: [
-        { name: 'Eixo1', max: 6500 },
-        { name: 'Eixo2', max: 16000 },
-        { name: 'Eixo3', max: 30000 },
-        { name: 'Eixo4', max: 38000 },
-        { name: 'Eixo5', max: 52000 },
-        { name: 'Eixo6', max: 25000 }
+        { name: topicos[idMateria][0], max: 5 },
+        { name: topicos[idMateria][1], max: 5 },
+        { name: topicos[idMateria][2], max: 5 },
+        { name: topicos[idMateria][3], max: 5 },
+        { name: topicos[idMateria][4], max: 5 }
       ]
     },
     series: [
@@ -69,7 +109,7 @@ function drawChart2() {
         type: 'radar',
         data: [
           {
-            value: [4000, 17000, 30000, 10000, 40000, 20000],
+            value: mediasMateria(idMateria, '3B'),
             name: 'Graficos'
           }
         ]
@@ -82,7 +122,7 @@ function drawChart2() {
   
 }
 
-function drawChart3() {
+function drawChart3(idMateria) {
 
   var chartDom = document.getElementById('grafico3');
   var myChart = echarts.init(chartDom);
@@ -90,7 +130,7 @@ function drawChart3() {
 
   option = {
     title: {
-      text: '3C INFO'
+      text: 'TADS'
     },
     legend: {
       data: ['Graficos']
@@ -98,12 +138,11 @@ function drawChart3() {
     radar: {
       // shape: 'circle',
       indicator: [
-        { name: 'Eixo1', max: 6500 },
-        { name: 'Eixo2', max: 16000 },
-        { name: 'Eixo3', max: 30000 },
-        { name: 'Eixo4', max: 38000 },
-        { name: 'Eixo5', max: 52000 },
-        { name: 'Eixo6', max: 25000 }
+        { name: topicos[idMateria][0], max: 5 },
+        { name: topicos[idMateria][1], max: 5 },
+        { name: topicos[idMateria][2], max: 5 },
+        { name: topicos[idMateria][3], max: 5 },
+        { name: topicos[idMateria][4], max: 5 }
       ]
     },
     series: [
@@ -112,7 +151,7 @@ function drawChart3() {
         type: 'radar',
         data: [
           {
-            value: [4000, 17000, 30000, 10000, 40000, 20000],
+            value: mediasMateria(idMateria, 'TADS'),
             name: 'Graficos'
           }
         ]
@@ -122,10 +161,22 @@ function drawChart3() {
 
   option && myChart.setOption(option);
 
-
 }
 
+drawChart1('biologia');
+drawChart2('biologia');
+drawChart3('biologia');
 
-drawChart1();
-drawChart2();
-drawChart3();
+const botoesMaterias = document.getElementsByClassName('botaoMateria')
+
+for (let botao = 0; botao < botoesMaterias.length; botao++) {
+
+  botoesMaterias.item(botao).addEventListener('click', (event) => {
+
+    drawChart1(event.target.id)
+    drawChart2(event.target.id)
+    drawChart3(event.target.id)
+
+  })
+
+}
