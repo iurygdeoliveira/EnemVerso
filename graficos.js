cbde8a428b3dcaca75dffd4d9730f384306382ef
Hiago-Oliver <hiago.oliver.12@gmail.com>
2024-06-20 11:10:45 -0300

    Gráficos correspondem à quantidade de turmas que enviaram JSON

--- graficos.js
+++ graficos.js
@@ -3,25 +3,27 @@ async function enviarMateria(materia){
     const totais = Array(5).fill(0); // Array para armazenar os totais de cada categoria
     const contadores = Array(5).fill(0); // Array para contar o número de elementos em cada categoria
 
-    jsonData.forEach(entrada => {
-        const slice_start = (Object.keys(entrada).indexOf(info_materia["1_Perg"]));
-        const slice_end = (Object.keys(entrada).indexOf(info_materia["1_Perg"])) + 15;
-        const valores = Object.values(entrada).slice(slice_start, slice_end); // Pega os valores das perguntas a partir da 9ª chave
+    let medias = [];
+    
+    for(let j = 0; j < jsonData.length; j++){
+      jsonData[j].forEach(entrada => {
+          const slice_start = (Object.keys(entrada).indexOf(info_materia[j]["1_Perg"]));
+          const slice_end = (Object.keys(entrada).indexOf(info_materia[j]["1_Perg"])) + 15;
+          const valores = Object.values(entrada).slice(slice_start, slice_end); // Pega os valores das perguntas a partir da 9ª chave
 
-        console.log(valores);
+          for (let i = 0; i < valores.length; i += 3) {
+              const categoriaIndex = Math.floor(i / 3);
+              const chunk = valores.slice(i, i + 3).map(Number);
 
-        for (let i = 0; i < valores.length; i += 3) {
-            const categoriaIndex = Math.floor(i / 3);
-            const chunk = valores.slice(i, i + 3).map(Number);
+              chunk.forEach(valor => {
+                  totais[categoriaIndex] += valor;
+                  contadores[categoriaIndex] += 1;
+              });
+          }
+      });
 
-            chunk.forEach(valor => {
-                totais[categoriaIndex] += valor;
-                contadores[categoriaIndex] += 1;
-            });
-        }
-    });
-
-    const medias = totais.map((total, index) => total / contadores[index]);
+      medias.push(totais.map((total, index) => total / contadores[index]));
+    }
     return medias;
   }
 
@@ -34,29 +36,31 @@ async function enviarMateria(materia){
 
   function buscarInfoMateria(materia){
     const resultado = info_materias.filter(item => item.Materia === materia);
-    console.log(resultado);
     return resultado;
   }
 
   let info_materia = buscarInfoMateria(materia);
 
-  let path = info_materia["Path"];
 
-  async function obterMediasDaTurma() {
-    let resultado = await getByPath(path);
 
-    let url = resultado.data.download_url;
+    async function obterMediasDaTurma() {
+      let mediasDaTurma = [];
+      for (let i = 0; i < info_materia.length; i++){
+        let path = info_materia[i]["Path"];
+        let resultado = await getByPath(path);
 
-    let dados = await fetch(url)
-        .then(
-            res => res.json()
-        );
+        let url = resultado.data.download_url;
 
-    const mediasDaTurma = calcularMediasDaTurma(Object.values(dados));
-    return mediasDaTurma;
+        let dados = await fetch(url)
+            .then(
+                res => res.json()
+            );
 
-  }
-
+        mediasDaTurma.push(Object.values(dados));
+      }
+      mediasDaTurma = calcularMediasDaTurma(mediasDaTurma);
+      return mediasDaTurma;
+    }
   async function getByPath(path) {
     return await executeRequest(path);
   }
@@ -77,6 +81,7 @@ async function enviarMateria(materia){
   // função de gerar os gráficos
   function criarGrafico(elementId, valores, titulo) {
   var dom = document.getElementById(elementId);
+  dom.style.display = 'block';
   var myChart = echarts.init(dom, null, {
     renderer: 'canvas',
     useDirtyRect: false
@@ -89,25 +94,25 @@ async function enviarMateria(materia){
       bottom: 0
     },
     legend: {
-      data: [info_materia["Materia"]]
+      data: [info_materia[0]["Materia"]]
     },
     radar: {
       indicator: [
-        { name: info_materia["Eixo1"], max: 5 },
-        { name: info_materia["Eixo2"], max: 5 },
-        { name: info_materia["Eixo3"], max: 5 },
-        { name: info_materia["Eixo4"], max: 5 },
-        { name: info_materia["Eixo5"], max: 5 }
+        { name: info_materia[0]["Eixo1"], max: 5 },
+        { name: info_materia[0]["Eixo2"], max: 5 },
+        { name: info_materia[0]["Eixo3"], max: 5 },
+        { name: info_materia[0]["Eixo4"], max: 5 },
+        { name: info_materia[0]["Eixo5"], max: 5 }
       ],
       center: ["50%", "50%"],
       radius: ["0%", "50%"]
     },
     series: [{
-      name: 'Áreas da língua inglesa',
+      name: info_materia[0]["Materia"],
       type: 'radar',
       data: [{
         value: valores,
-        name: info_materia["Materia"]
+        name: info_materia[0]["Materia"]
       }]
     }]
   };
@@ -125,16 +130,21 @@ async function enviarMateria(materia){
   obterMediasDaTurma()
   .then(mediasDaTurma => {
     if (mediasDaTurma) {
-      const valores = [
-        mediasDaTurma[0],
-        mediasDaTurma[1],
-        mediasDaTurma[2],
-        mediasDaTurma[3],
-        mediasDaTurma[4]
-      ];
-      criarGrafico('graficos_3A', valores, '3A');
-      criarGrafico('graficos_3B', valores, '3B');
-      criarGrafico('graficos_TADS', valores, 'TADS');
+      for (let i = 1; i <= 3; i++){
+        document.getElementById(`grafico${i}`).style.display = 'none';
+      }
+      for(let i = 0; i < mediasDaTurma.length; i++){
+        let valores = [
+          mediasDaTurma[i][0],
+          mediasDaTurma[i][1],
+          mediasDaTurma[i][2],
+          mediasDaTurma[i][3],
+          mediasDaTurma[i][4]
+        ];
+        console.log(valores);
+        console.log(`grafico${i + 1}`);
+        criarGrafico(`grafico${i + 1}`, valores, info_materia[i]["Turma"]);
+      }
     }
   })
   .catch(error => {
